61cb62a8a804f9e4dd119f061d0de683478f85b0
Abhi-1301 <90414121+Abhi-1301@users.noreply.github.com>
2022-04-28 15:44:58 +0530

    Added

--- Automatic_Elevator_Control_System/src/frontend/emergency_id.java
+++ Automatic_Elevator_Control_System/src/frontend/emergency_id.java
@@ -1,6 +1,8 @@
 package frontend;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -41,26 +43,30 @@ public class emergency_id extends JFrame {
 	 * Create the frame.
 	 */
 	public emergency_id() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 508, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(224, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		add_text = new JTextField();
-		add_text.setBounds(10, 25, 369, 36);
+		add_text.setBounds(10, 25, 369, 98);
 		contentPane.add(add_text);
 		add_text.setColumns(10);
 
 		subtract_text = new JTextField();
 		subtract_text.setColumns(10);
-		subtract_text.setBounds(10, 97, 369, 144);
+		subtract_text.setBounds(10, 143, 369, 98);
 		contentPane.add(subtract_text);
 
 		List<String> em_id = new ArrayList();
 
-		JButton add = new JButton("Add");
+		JButton add = new JButton("+");
+		add.setBackground(new Color(152, 251, 152));
+		add.setFont(new Font("Rockwell", Font.PLAIN, 23));
 		add.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String s = add_text.getText();
@@ -84,10 +90,12 @@ public class emergency_id extends JFrame {
 				}
 			}
 		});
-		add.setBounds(399, 32, 85, 21);
+		add.setBounds(418, 52, 50, 50);
 		contentPane.add(add);
 
-		JButton subtract = new JButton("Subtract");
+		JButton subtract = new JButton("-");
+		subtract.setBackground(new Color(220, 20, 60));
+		subtract.setFont(new Font("Rockwell", Font.PLAIN, 23));
 		subtract.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int index = em_id.indexOf(subtract_text.getText());
@@ -102,7 +110,7 @@ public class emergency_id extends JFrame {
 				}
 			}
 		});
-		subtract.setBounds(399, 97, 85, 21);
+		subtract.setBounds(418, 161, 50, 50);
 		contentPane.add(subtract);
 	}
 }
--- Automatic_Elevator_Control_System/src/frontend/mainpage.java
+++ Automatic_Elevator_Control_System/src/frontend/mainpage.java
@@ -44,41 +44,47 @@ public class mainpage extends JFrame {
 	 * Create the frame.
 	 */
 	public mainpage() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(224, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		panel.setBackground(Color.YELLOW);
+		panel.setBackground(new Color(224, 255, 255));
 		panel.setBounds(0, 0, 204, 356);
 		contentPane.add(panel);
 
 		JLabel lblNewLabel_1 = new JLabel("AUTOMATIC");
 		lblNewLabel_1.setVerticalAlignment(SwingConstants.BOTTOM);
 		panel.add(lblNewLabel_1);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel_1.setFont(new Font("Rockwell", Font.PLAIN, 30));
 
 		JLabel lblNewLabel_2 = new JLabel("ELEVATOR");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel_2.setFont(new Font("Rockwell", Font.PLAIN, 30));
 		panel.add(lblNewLabel_2);
 
 		JLabel lblNewLabel = new JLabel("SYSTEM");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel.setFont(new Font("Rockwell", Font.PLAIN, 30));
 		panel.add(lblNewLabel);
 
 		JButton configure = new JButton("Configure");
+		configure.setForeground(new Color(255, 255, 255));
+		configure.setBackground(new Color(0, 0, 128));
 		configure.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				configure btn1 = new configure();
 				btn1.main(null);
 			}
 		});
-		configure.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		configure.setFont(new Font("Rockwell", Font.PLAIN, 20));
 		configure.setBounds(258, 43, 154, 33);
 		contentPane.add(configure);
 
 		JButton initiate = new JButton("Initiate");
+		initiate.setForeground(new Color(255, 255, 255));
+		initiate.setBackground(new Color(0, 0, 128));
 		initiate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -90,18 +96,20 @@ public class mainpage extends JFrame {
 				}
 			}
 		});
-		initiate.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		initiate.setFont(new Font("Rockwell", Font.PLAIN, 20));
 		initiate.setBounds(258, 117, 154, 33);
 		contentPane.add(initiate);
 
 		JButton report = new JButton("Log Report");
+		report.setForeground(new Color(255, 255, 255));
+		report.setBackground(new Color(0, 0, 128));
 		report.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				log btn1 = new log();
 				// btn1.main(null);
 			}
 		});
-		report.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		report.setFont(new Font("Rockwell", Font.PLAIN, 20));
 		report.setBounds(258, 191, 154, 33);
 		contentPane.add(report);
 
